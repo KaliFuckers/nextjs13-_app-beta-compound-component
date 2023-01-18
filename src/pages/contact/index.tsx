c9b8b040bf993cbb2f4eee854f921e43f3f8dbe0
shammael <shammamnd2015@gmail.com>
2023-01-17 17:56:51 -0800

    fix: fix layout export page bug

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,5 +1,13 @@
+import MainLayout from "@/layouts/main";
+import { ReactElement } from "react";
 import type { NextPageWithLayout } from "../_app";
 
 const ContactPage: NextPageWithLayout = () => {
   return <h1>Contact Page</h1>;
 };
+
+ContactPage.getLayout = function getLayout(page: ReactElement) {
+  return <MainLayout>{page}</MainLayout>;
+};
+
+export default ContactPage;
